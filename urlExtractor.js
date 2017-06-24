708e1b4d009235588ac3d68cf4c9a6413645d34c
joaom182 <joaom182@hotmail.com>
2017-06-24 18:34:24 -0300

    Ajust identation and remove arrow function to make compatibility with old browsers.

--- urlExtractor.js
+++ urlExtractor.js
@@ -1,30 +1,30 @@
 function extractUrls(text) {
-  var words = text
-    .replace(/([\s\(\)\[\]<>"'])/g, "\0$1\0")
-    .replace(/([?;:,.!]+)(?=(\0|$|\s))/g, "\0$1\0")
-    .split("\0");
+    var words = text
+        .replace(/([\s\(\)\[\]<>"'])/g, "\0$1\0")
+        .replace(/([?;:,.!]+)(?=(\0|$|\s))/g, "\0$1\0")
+        .split("\0");
 
-  var validUrls = [];
+    var validUrls = [];
 
-  words.forEach((word) => {
-    if (!word)
-      return;
+    words.forEach(function (word) {
+        if (!word)
+            return;
 
-    var urlRegex = /(ftp: \/\/|www\.|https?:\/\/){1}[a-zA-Z0-9u00a1-\uffff0-]{2,}\.[a-zA-Z0-9u00a1-\uffff0-]{2,}(\S*)/g;
-    if (urlRegex.test(word))
-      return validUrls.push(word);
+        var urlRegex = /(ftp: \/\/|www\.|https?:\/\/){1}[a-zA-Z0-9u00a1-\uffff0-]{2,}\.[a-zA-Z0-9u00a1-\uffff0-]{2,}(\S*)/g;
+        if (urlRegex.test(word))
+            return validUrls.push(word);
 
-    var splited = word.split('.');
-    var isValidUrl = (splited.length > 1 &&
-      (splited[splited.length - 1].length >= 2 ||
-        (splited.length > 2 &&
-          splited[splited.length - 1].length == 0)))
+        var splited = word.split('.');
+        var isValidUrl = (splited.length > 1 &&
+            (splited[splited.length - 1].length >= 2 ||
+                (splited.length > 2 &&
+                    splited[splited.length - 1].length == 0)))
 
-    if (isValidUrl)
-      return validUrls.push(word);
-  });
+        if (isValidUrl)
+            return validUrls.push(word);
+    });
 
-  return validUrls;
+    return validUrls;
 }
 
 module.exports = extractUrls;
